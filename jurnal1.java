2f5ea8c4cf3297cbd672c906dcffcb9307959c10
IPutuNisaSuryaPrabana <nisasp27@gmail.com>
2017-04-06 09:29:44 +0700

    Update jurnal1.java

--- jurnal1.java
+++ jurnal1.java
@@ -1,20 +1,21 @@
 public class jurnal1{
 	
-	public static void main(String [] args){
-		Scanner show = new Scanner(System.in);
-		System.out.print("Masukkan Jumlah Deret Fibonacci: ");
-		int i = show.nextInt();
-		long fib[] = new long[i];
-		
-		fib[0] = 1;
-		fib[1] = 1;
-		
-		for(int n = 2; n < i; n++){
-			fib[n] = fib[n-1] + fib[n-2];
-		}
-		
-		for(int n = 0; n < i; n++){
-			System.out.print(fib[n] + " ");
-		}
-	}
+	static int fib(int n){
+        int f[] = new int[n+1];
+        int i;
+        
+        f[0] = 0;
+        f[1] = 1;
+        
+        for(i=2; i<=n; i++){
+            f[i] = f[i-1] +f[i-2];
+        }
+        return f[n];
+    }
+	
+    public static void main(String [] args){
+	int n = 7;
+        System.out.print("Hasil Fibonacci: ");
+        System.out.print(fib(n));
+    }
 }
